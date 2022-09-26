4e5ea5052fe7c4fe7d38a4404c57fffcaa8924a1
ciprianoaljhon <ciprianoaljhons.7@gmail.com>
2022-09-26 11:55:04 +0800

    update

--- src/script.js
+++ src/script.js
@@ -1,11 +1,13 @@
 const menuBtn = document.querySelector('[data-toggle="menu"]');
       hamburgerMenu = document.querySelector('[data-type="hamburger"]')
       closeBtn = document.querySelector('[data-type="close"]')
-
+      header = document.querySelector('header')
+      navLinks = document.querySelectorAll('[data-type="nav-link"]')
 
 menuBtn.addEventListener('pointerdown', () => {
     hamburgerMenu.classList.toggle('rotate-[720deg]');
     hamburgerMenu.classList.toggle('opacity-0')
     closeBtn.classList.toggle('opacity-0');
-    closeBtn.classList.toggle('rotate-360deg');
+    closeBtn.classList.toggle('rotate-[720deg]'); 
+    header.classList.toggle('show-nav');
 })
